21e2cec1ffeaa611de0a1c77dd3a63ae8bdd8da3
kbogar <krisztianbogar@gmail.com>
2023-01-10 11:01:09 +0000

    Add imports and create two initial functions

---
+++ run.py
@@ -0,0 +1,23 @@
+# Importing libraries
+import random
+import string
+# Imports from other files
+from words import words_list
+from hangmans import hangman_stages
+
+#
+def get_word(words_list):
+    """
+    This function checks if the word is correct or not
+    """
+    pass
+
+
+def main_hangman(words_list):
+    """
+    Main function for testing
+    """
+    pass
+
+main_hangman(words_list)
+
