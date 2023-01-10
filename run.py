b3a1787e3e314bd4195daa43a7d5391953634ffc
kbogar <krisztianbogar@gmail.com>
2023-01-10 12:45:21 +0000

    Add while loop with if statement to main function to check played letters and to write out hangman stages not finished

--- run.py
+++ run.py
@@ -27,6 +27,21 @@ def main_hangman(words_list):
     letters_from_words = set(word)
     # Return all Ascii letters in uppercase and store in variable
     abc = set(string.ascii_uppercase)
+    # Empty set of used letters to add them later
+    used_letters = set()
+
+    lives_left = 7
+    # Inputs from user
+    while len(letters_from_words) > 0 and lives_left > 0:
+        print(f'You have {lives_left} lives left.')
+        print('Used letters: ', ' '.join(used_letters))
+
+        # Writes out the played letters that in used letters
+        # or we write out '-'
+        # prints out the hangman stages with lives left
+        listwd = [letter if letter in used_letters else '-' for letter in word]
+        print(hangman_stages[lives_left])
+        print('Current word: ', ' '.join(listwd))
 
 
 main_hangman(words_list)
