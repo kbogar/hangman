84ad1a4ed90c75c53eba8929051dec8dc0ca462a
kbogar <krisztianbogar@gmail.com>
2023-01-10 11:38:18 +0000

    Add get random word and create generate letters from word to a function

--- run.py
+++ run.py
@@ -9,15 +9,25 @@ from hangmans import hangman_stages
 def get_word(words_list):
     """
     This function checks if the word is correct or not
+    and gets a random word with uppercase
     """
-    pass
+    word = random.choice(words_list)
+    while ('-' in word) or (' ' in word):
+        word = random.choice(words_list)
+
+    return word.upper()
 
 
 def main_hangman(words_list):
     """
     Main function for testing
     """
-    pass
+    word = get_word(words_list)
+    # Variable to generate letters from words
+    letters_from_words = set(word)
+    # Return all Ascii letters in uppercase and store in variable
+    abc = set(string.ascii_uppercase)
+
 
 main_hangman(words_list)
 
